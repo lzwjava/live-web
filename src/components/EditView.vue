a97265ec72a09bec9ac2956819b7102f176e7aff
lzwjava <lzwjava@gmail.com>
2016-08-22 20:16:04 +0800

    tabs to spaces

--- src/components/EditView.vue
+++ src/components/EditView.vue
@@ -89,10 +89,10 @@ export default {
     },
     fetchUser() {
       this.$http.get('self').then((res) => {
-				if (util.filterError(this, res)) {
-					debug(res.data.result)
-				}
-			}, util.httpErrorFn(this))
+        if (util.filterError(this, res)) {
+          debug(res.data.result)
+        }
+      }, util.httpErrorFn(this))
     },
     fetchLive() {
       this.$http.get('lives/lastPrepare').then((res) => {
@@ -147,74 +147,74 @@ export default {
     },
     initQiniu() {
       var component = this;
-			this.$http.get('files/uptoken').then((res) => {
-				if (util.filterError(this, res)) {
-					debug('qiniu token %j', res.data);
+      this.$http.get('files/uptoken').then((res) => {
+        if (util.filterError(this, res)) {
+          debug('qiniu token %j', res.data);
           var result = res.data.result;
-					var uptoken = result.uptoken;
-					var bucketUrl = result.bucketUrl;
+          var uptoken = result.uptoken;
+          var bucketUrl = result.bucketUrl;
           var key =result.key;
-					var uploader = Qiniu.uploader({
-					    runtimes: 'html5,flash,html4',    //上传模式,依次退化
-					    browse_button: 'pickfiles',       //上传选择的点选按钮，**必需**
-					    uptoken_url: 'useless',
-					    uptoken: uptoken,
-					    domain: bucketUrl,
-					    flash_swf_url: 'js/plupload/Moxie.swf',
-					    unique_names: false,
+          var uploader = Qiniu.uploader({
+              runtimes: 'html5,flash,html4',    //上传模式,依次退化
+              browse_button: 'pickfiles',       //上传选择的点选按钮，**必需**
+              uptoken_url: 'useless',
+              uptoken: uptoken,
+              domain: bucketUrl,
+              flash_swf_url: 'js/plupload/Moxie.swf',
+              unique_names: false,
               save_key: false,
-					    get_new_uptoken: false,           //设置上传文件的时候是否每次都重新获取新的token
-					    container: 'upload-container',    //上传区域DOM ID，默认是browser_button的父元素，
-					    max_file_size: '100mb',           //最大文件体积限制
-					    max_retries: 3,                   //上传失败最大重试次数
-					    dragdrop: false,                  //开启可拖曳上传
-					    drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-					    chunk_size: '4mb',                //分块上传时，每片的体积
-					    auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传,
-					    init: {
-					        'FilesAdded': function(up, files) {
-					        },
-					        'BeforeUpload': function(up, file) {
-					        },
-					        'UploadProgress': function(up, file) {
-					        },
-					        'FileUploaded': function(up, file, info) {
-					               // info:
-					               // {
-					               //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
-					               //    "key": "gogopher.jpg"
-					               //  }
-					               var res = JSON.parse(info);
-					               var domain = up.getOption('domain');
-					               // 从第10个找，跳过之前的 http:// ，看看是不是 / 结尾
-					               if (domain.indexOf('/', 10) == -1) {
-					               	  domain += '/';
-					               }
-					               var sourceLink = domain + res.key;
-					               debug('sourceLink: %j', sourceLink);
+              get_new_uptoken: false,           //设置上传文件的时候是否每次都重新获取新的token
+              container: 'upload-container',    //上传区域DOM ID，默认是browser_button的父元素，
+              max_file_size: '100mb',           //最大文件体积限制
+              max_retries: 3,                   //上传失败最大重试次数
+              dragdrop: false,                  //开启可拖曳上传
+              drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+              chunk_size: '4mb',                //分块上传时，每片的体积
+              auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传,
+              init: {
+                  'FilesAdded': function(up, files) {
+                  },
+                  'BeforeUpload': function(up, file) {
+                  },
+                  'UploadProgress': function(up, file) {
+                  },
+                  'FileUploaded': function(up, file, info) {
+                         // info:
+                         // {
+                         //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                         //    "key": "gogopher.jpg"
+                         //  }
+                         var res = JSON.parse(info);
+                         var domain = up.getOption('domain');
+                         // 从第10个找，跳过之前的 http:// ，看看是不是 / 结尾
+                         if (domain.indexOf('/', 10) == -1) {
+                             domain += '/';
+                         }
+                         var sourceLink = domain + res.key;
+                         debug('sourceLink: %j', sourceLink);
                          component.updateCover(sourceLink)
                          // update
-					              //  component.avatarUrl = sourceLink;
-					              //  component.updateUser(null, () => {
-					              //  	  // todo，component 没有进行更新的问题
-					              //  	  window.location = '/setting.html';
-					              //  });
-					        },
-					        'Error': function(up, err, errTip) {
-					               debug('qiniu error %j errTip %j', err, errTip);
-					               util.show(component, 'error', errTip);
-					        },
-					        'UploadComplete': function() {
-					        },
-					        'Key': function(up, file) {
-					            // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
-					            // 该配置必须要在 unique_names: false , save_key: false 时才生效
-					            return result.key;
-					        }
-					    }
-					});
-				}
-			}, util.httpErrorFn(this))
+                        //  component.avatarUrl = sourceLink;
+                        //  component.updateUser(null, () => {
+                        //      // todo，component 没有进行更新的问题
+                        //      window.location = '/setting.html';
+                        //  });
+                  },
+                  'Error': function(up, err, errTip) {
+                         debug('qiniu error %j errTip %j', err, errTip);
+                         util.show(component, 'error', errTip);
+                  },
+                  'UploadComplete': function() {
+                  },
+                  'Key': function(up, file) {
+                      // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+                      // 该配置必须要在 unique_names: false , save_key: false 时才生效
+                      return result.key;
+                  }
+              }
+          });
+        }
+      }, util.httpErrorFn(this))
     }
   }
 }
@@ -282,6 +282,6 @@ export default {
     #upload-container
       img
         width 200px
-        margin-top 10px        
+        margin-top 10px
 
 </style>
