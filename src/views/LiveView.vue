94458bb0ed796388a519665b9573d0e19faaab92
lzwjava <lzwjava@gmail.com>
2016-10-24 07:41:24 +0800

    min-height

--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -388,7 +388,7 @@ export default {
     float left
     .player-area
       width 100%
-      height 100%
+      min-height 500px
       margin 0 auto
       position relative
       background-color #383838
