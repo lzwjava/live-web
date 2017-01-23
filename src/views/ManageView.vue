5be17cd160d438f51971d9ab529dcf01748fa624
lzwjava <lzwjava@gmail.com>
2017-01-24 00:49:38 +0800

    only static

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -125,7 +125,7 @@ export default {
     data({to}) {
       var params = this.$route.params
       this.liveId = params.liveId
-      // this.fetchLive()
+      this.fetchLive()
     }
   },
   created() {
